8463f51a1b0b489df1f1dbaf9516c2edeb190436
Anastasiia Makhynia <mah5129@gmail.com>
2024-04-07 21:01:26 +0300

    add input and output functions

---
+++ app/io/input.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def input_text_from_console():
+    pass
+
+def read_text_from_file(file_path):
+    pass
+
+
+def read_text_from_file_with_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def output_text_to_console(text):
+    pass
+
+
+def write_text_to_file(text, file_path):
+    pass
+
+
+def write_text_to_file_with_pandas(text, file_path):
+    pass
